3aa5071378fffb8c56cafa3425bb6c2ebdf845d5
Pavel <malinatrashh@gmail.com>
2024-11-19 21:45:10 +0800

    task-2 (point-3)

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -6,26 +6,70 @@ class PrintEditionItem {
         this.pagesCount = pagesCount; //кол-во страниц
         this.state = 100; //состояние книг
         this.type = null;
-
-        fix() {
-            this.state = 
-        }
     } 
+    
+    fix() {
+        this.state *= 1.5;
+    }
+
+    setState(state) {
+        if(state < 0) {
+            this.state = 0;
+        } else if(state > 100) {
+            this.state = 100;
+        } else {
+            this.state = state;
+        }
+    }
+
+    getState() {
+        return this.state;
+    }
 }
 
-class PrintEditionItem extends Magazine {
-    super(name);
-    super(releaseDate);
-    super(pagesCount);
-    super(state);
-    type = 'magazine';
-    
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = 'magazine';
+    }
 }
+
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = 'book';
+    }
+}
+
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = 'novel';
+    }
+}
+class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount, author) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = 'fantastic';
+    }
+}
+class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = 'detective';
+    }
+}
+
+
+
 
 // Task-2
-class Library(nameOfTheLibrary) {
-    this.nameOfTheLibrary = nameOfTheLibrary;
-    this.books = [];
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
 
     addBook(book) {
         if(state > 30) {
@@ -33,7 +77,18 @@ class Library(nameOfTheLibrary) {
         }
     }
 
-    findBookBy(type, value)
+    findBookBy(type, value) {
+        this.books.filter(book => {
+            return book[type] == value
+        }) 
+
+        
+
+    }
+
+    giveBookByName(bookName) {
+
+    }
 }
     
 
