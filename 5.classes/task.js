e7f44a537b5018830ee83022be75e57b9dd03657
Zaraza <antipovaanostasia@gmail.com>
2024-11-19 17:58:45 +0800

    unfinished

---
+++ 5.classes/task.js
@@ -0,0 +1,29 @@
+// // Task-1
+// class PrintEditionItem {
+//     constructor(name, releaseDate, pagesCount) {
+//         this.name = name;
+//         this.releaseDate = releaseDate; // дата выпуска
+//         this.pagesCount = pagesCount; //кол-во страниц
+//         this.state = 100; //состояние книг
+//         this.type = null;
+
+//         fix() {
+//             this.state = 
+//         }
+//     } 
+// }
+
+// class PrintEditionItem extends Magazine {
+//     super(name);
+//     super(releaseDate);
+//     super(pagesCount);
+//     super(state);
+//     type = 'magazine';
+    
+// }
+
+// Task-2
+class Library(nameOfTheLibrary, books)
+    this.na
+
+// Task-3
