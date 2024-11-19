39188f30d15dbbf23ea624c5c32482f4c4debae2
Zaraza <antipovaanostasia@gmail.com>
2024-11-19 18:02:34 +0800

    unfinished-2

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,29 +1,40 @@
-// // Task-1
-// class PrintEditionItem {
-//     constructor(name, releaseDate, pagesCount) {
-//         this.name = name;
-//         this.releaseDate = releaseDate; // дата выпуска
-//         this.pagesCount = pagesCount; //кол-во страниц
-//         this.state = 100; //состояние книг
-//         this.type = null;
+// Task-1
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate; // дата выпуска
+        this.pagesCount = pagesCount; //кол-во страниц
+        this.state = 100; //состояние книг
+        this.type = null;
 
-//         fix() {
-//             this.state = 
-//         }
-//     } 
-// }
+        fix() {
+            this.state = 
+        }
+    } 
+}
 
-// class PrintEditionItem extends Magazine {
-//     super(name);
-//     super(releaseDate);
-//     super(pagesCount);
-//     super(state);
-//     type = 'magazine';
+class PrintEditionItem extends Magazine {
+    super(name);
+    super(releaseDate);
+    super(pagesCount);
+    super(state);
+    type = 'magazine';
     
-// }
+}
 
 // Task-2
-class Library(nameOfTheLibrary, books)
-    this.na
+class Library(nameOfTheLibrary) {
+    this.nameOfTheLibrary = nameOfTheLibrary;
+    this.books = [];
+
+    addBook(book) {
+        if(state > 30) {
+            books.push(book)
+        }
+    }
+
+    findBookBy(type, value)
+}
+    
 
 // Task-3
